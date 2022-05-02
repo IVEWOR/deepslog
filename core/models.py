55626ce3e49675c8421955304b427dafda8a7fda
IVEWOR <howdy@deepslog.com>
2022-05-03 01:10:12 +0530

    models.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+
+# Base
+class BaseOptions(models.Model):
+    title = models.CharField(max_length=80)
+    slug = models.SlugField(blank=True)
+    image = models.ImageField(upload_to="images", blank=True, null=True)
+    image_caption = models.CharField(max_length=80, blank=True)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+    meta_description = models.TextField(blank=True)
+
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(BaseOptions, self).save(*args, **kwargs)
+
+
+# tags
+class Tags(BaseOptions):
+    pass
+
+    def __str__(self):
+        return self.title
+
+
+# Topic
+class Topic(BaseOptions):
+    pass
+
+    def __str__(self):
+        return self.title
+
+
+# blog
+class Blog(BaseOptions):
+    body = models.TextField()
+    category = models.ForeignKey(Topic, on_delete=models.SET_NULL, blank=True, null=True, related_name="blog_topic")
+    tags = models.ManyToManyField(Tags, blank=True, related_name="blog_tags")
+
+    def __str__(self):
+        return self.title
+
+
--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from core import views
 
+
 urlpatterns = [
     path("", views.index, name="index"),
 ]
