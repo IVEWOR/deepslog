2392b20abf95e68bc377c43ea499cea6c6ae3269
IVEWOR <howdy@deepslog.com>
2022-05-04 05:26:45 +0530

    many channges

--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from core.models import Blog
 
 
 def index(request):
+    blog = Blog.objects.all()
     template_name = "index.html"
-    return render(request, template_name)
+    return render(request, template_name, context={"blog": blog})
