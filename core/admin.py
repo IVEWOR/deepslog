1226deff221f7254020f3531e398ed40f0d62b59
IVEWOR <howdy@deepslog.com>
2022-05-04 01:39:10 +0530

    some changes

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from core.models import Blog, Topic, Tags
 
-# Register your models here.
+admin.site.register(Blog)
+admin.site.register(Topic)
+admin.site.register(Tags)
+
